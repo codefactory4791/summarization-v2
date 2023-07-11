b2dcd72e62feccd7beece59ea40be566bf9a255a
codefactory4791 <shekhart91@gmail.com>
2023-07-11 10:43:22 -0400

    adding new main.py

--- app/main.py
+++ app/main.py
@@ -21,28 +21,39 @@ max_input_length = 1024
 max_target_length = 128
 
 
-model_path = os.path.abspath("/app/app/model/model_artifacts")
-model_checkpoint ='t5-small'
 
-fs = gcsfs.GCSFileSystem(project='call-summarizatiion')
-with fs.open('summarization_bucket_2023/pytorch_model.bin', 'rb') as f:
-    print("writing model...")
-    full_model_path = f"{model_path}/pytorch_model.bin"
-    open(full_model_path, 'wb').write(f.read())
+def download_artifacts():
 
+    model_path = os.path.abspath("/app/app/model/model_artifacts")
+    model_checkpoint ='t5-small'
 
-if os.path.isdir(model_path):
-
-
-    print("Loading Model")
-    t5_model = AutoModelForSeq2SeqLM.from_pretrained(full_model_path)
-    print("Model Loaded")
-else:
-    print("No Model Artifacts Found")
-    tf_model = None
+    print("Downloading Model")
+    bucket_name = "summarization_bucket_2023"
+    destination_file_name = f"{model_path}/pytorch_model.bin"
+    storage_client = storage.Client("call-summarizatiion")
+    bucket = storage_client.get_bucket(bucket_name)
+    blob = bucket.blob("pytorch_model.bin")
+    blob.download_to_filename(destination_file_name)
+    print("Model Downloaded")
 
 
-tokenizer = AutoTokenizer.from_pretrained(model_checkpoint)
+    if os.path.isdir(model_path):
+
+
+        print("Loading Model")
+        t5_model = AutoModelForSeq2SeqLM.from_pretrained(model_path)
+        print("Model Loaded")
+    else:
+        print("No Model Artifacts Found")
+        tf_model = None
+
+
+    tokenizer = AutoTokenizer.from_pretrained(model_checkpoint)
+
+    return tf_model,tokenizer
+
+
+tf_model,tokenizer = download_artifacts()
 
 
 # input is of type list of string List[str], where each item in the list is an article which needs to be summarized
